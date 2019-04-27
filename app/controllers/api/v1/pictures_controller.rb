64edd46c28318940f4fea8fb6ec4f1a5c7a07589
Neylanio Soares <neylaniogomes@gmail.com>
2019-04-27 11:36:13 -0300

    Vai dar certo, em nome de Deus

---
+++ app/controllers/api/v1/pictures_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::PicturesController < ApplicationController
+	def create
+		picture = Picture.new create_picture_params
+		picture.image.attach create_picture_params[:image] if create_picture_params[:image]
+		if picture.save
+			render json: picture, status: :created
+		else
+			render json: { errors: picture.errors }, status: :unprocessable_entity
+		end
+	end
+
+	def index
+		render json: Picture.all.map(&:image_url)
+	end
+
+	private
+
+	def create_picture_params
+		params.permit(:name, :image)
+	end
+end
